52e70184f589f73d4da3893b9d7133d3ecc5a5e1
trinidad <nestrimo@gmail.com>
2020-02-19 08:56:59 -0600

    class get

---
+++ src/clases/Clases.java
@@ -0,0 +1,11 @@
+
+package clases;
+public class Clases {    
+    public static void main(String[] args) {
+       Persona juan;//Declaracion
+       juan=new Persona();//Insta
+       juan.setNombre("Juan");
+       juan.cualEsTuNombre();
+       
+    }    
+}
---
+++ src/clases/Persona.java
@@ -0,0 +1,28 @@
+package clases;
+public class Persona {
+    //
+    private String Nombre;
+    private int Edad;
+    private char Sexo;
+    private String ColorPiel;
+    
+    Persona(){    
+    }
+    
+    public void setNombre(String Nombre){
+        this.Nombre=Nombre;
+    }
+    
+    public void cualEsTuNombre(){        
+        System.out.println("Mi nombre es: "+this.Nombre);
+    }
+    
+    public void caminar(){    
+        System.out.println("Estoy caminando");
+    }
+    private  void hablar(){    
+        System.out.println("blah..");
+    }
+    
+    
+}
